3d11f407db8a473ab677e0b3304614a2855da6c6
manjinder <manjinder@jcsoftwaresolution.com>
2024-03-29 17:36:41 +0530

    cjhanges

--- src/app/Auth/forgot/forgot.component.ts
+++ src/app/Auth/forgot/forgot.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BehaviourService } from '../../Shared/behaviour.service';
+import { AppService } from '../../Shared/app.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-forgot',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './forgot.component.html',
   styleUrl: './forgot.component.scss'
 })
 export class ForgotComponent {
+  submitted:any=false
+  forgot:FormGroup
 
+  constructor(private fb:FormBuilder,private bs:BehaviourService ,private appservice:AppService){
+this.forgot=fb.group({
+  email: ['', [Validators.required, Validators.email, Validators.pattern("^[a-zA-Z0-9._%. +-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{1,4}.$")]],
+})
+
+
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.forgot.controls;
+  }
+
+  onSubmit(){
+    this.submitted=true
+
+  }
 }
